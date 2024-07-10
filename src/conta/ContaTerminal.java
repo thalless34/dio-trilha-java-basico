c013375147355c8a9f63c8be0604a13083277802
thalless34 <th_allessobral0@icloud.com>
2024-07-10 18:47:36 -0300

    projeto proposto

--- src/conta/ContaTerminal.java
+++ src/conta/ContaTerminal.java
@@ -1,5 +1,31 @@
 package conta;
 
-public class ContaTerminal {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class ContaTerminal {
+	
+	public static void main(String[] args) {
+		
+		String nomeCliente;
+		String agencia;
+		int numConta;
+		double saldo;
+		
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Digite o numero da agencia: ");
+		agencia = scanner.next();
+		System.out.println("digite o numero da conta: ");
+		numConta = scanner.nextInt();
+		System.out.println("digite o nome do cliente: ");
+		nomeCliente = scanner.next();
+		System.out.println("digite valor a ser depositado: ");
+		saldo = scanner.nextDouble();
+		
+		System.out.printf("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponivel para saque.");
+		
+		scanner.close();
+		
+	}
 }
